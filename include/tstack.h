79419524b5c0230bac4131fd950642245bf2e56c
feneboy3 <dankala2007@gmail.com>
2026-04-19 21:42:08 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,31 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T data[size];
+    int top_index;
+
+ public:
+    TStack() {
+        top_index = -1;
+    }
+
+    void push(T value) {
+        top_index++;
+        data[top_index] = value;
+    }
+
+    void pop() {
+        top_index--;
+    }
+
+    T top() {
+        return data[top_index];
+    }
+
+    bool empty() {
+        return top_index == -1;
+    }
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif
